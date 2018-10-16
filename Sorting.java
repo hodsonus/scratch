5bef7093735604118a230ca798935361c7d1522f
John Hodson <jhodson@eufl.edu>
2018-10-16 12:53:28 -0400

    finish selection, insertion, bubble

--- Sorting.java
+++ Sorting.java
@@ -1,50 +1,111 @@
+import java.util.*;
+
 public class Sorting {
 	
 	public static void main(String[] args) {
 
+		List<Integer> nums = new ArrayList<Integer>(Arrays.asList(9, 6, 3, 2, 1, 4, 3, 7));
+		insertionSort(nums);
+
+		for(int num : nums) {
+			System.out.println(num + " ");
+		}
 	}
 
+//TODO
 	public static void iterativeMergeSort() {
 
 	}
 
+//TODO
 	public static void recursiveMergeSort() {
 
 	}
 
+//TODO
 	public static void merge() {
 
 	}
 
+//TODO
 	public static void countingSort() {
 
 	}
 
+//TODO
 	public static void iterativeQuickSort() {
 
 	}
 
+//TODO
 	public static void recursiveQuickSort() {
 
 	}
 
+//TODO
 	public static void partition() {
 
 	}
 
-	public static void bubbleSort() {
+	public static void bubbleSort(List<Integer> nums) {
 
+		boolean swapped = true;
+		while (swapped) {
+			swapped = false;
+			for (int i = 1; i < nums.size(); i++) {
+				if (nums.get(i-1) > nums.get(i)) {
+					swapped = true;
+					swap(nums, i, i-1);
+				}
+			}
+		}
 	}
 
-	public static void insertionSort() {
+	public static void insertionSort(List<Integer> nums) {
 
+
+		for (int i = 1; i < nums.size(); i++) {
+			//the number to place into the sorted array is i
+
+			int j = i-1;
+			int toInsert = nums.get(i);
+
+			while (j>=0 && nums.get(j) > toInsert) {
+				nums.set(j+1, nums.get(j));
+				j--;
+			}
+
+			 nums.set(j+1, toInsert);
+		}
 	}
 
-	public static void selectionSort() {
+	public static void selectionSort(List<Integer> nums) {
 
+		int min;
+
+		for (int i = 0; i < nums.size()-1; i++) {
+
+			min = i;
+			for (int j = i; j < nums.size(); j++) {
+
+				if (nums.get(j) < nums.get(min)) {
+					min = j;
+				}
+			}
+
+			swap(nums, i, min);
+		}
 	}
 
+//TODO
 	public static void heapSort() {
-	
+
+	}
+
+	public static void swap(List<Integer> nums, int i, int j) {
+
+		int temp = nums.get(j);
+		nums.set(j, nums.get(i));
+		nums.set(i, temp);
 	}
 }
